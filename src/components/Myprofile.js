f917cd7353f438242db4f43801814a11bf8928c6
natanimabesha <natanimabesha@www.zhewutech.com>
2022-11-16 14:09:08 +0300

    Fix my profile missions

--- src/components/Myprofile.js
+++ src/components/Myprofile.js
@@ -5,23 +5,30 @@ import { useSelector } from 'react-redux';
 
 const Myprofile = () => {
   const rockets = useSelector((state) => state.rockets).filter((val) => val.reserved === true);
+  const missions = useSelector((state) => state.missions).filter((mission) => mission.member === true);
 
   return (
     <section className="section-containers section-rockets profile__container">
       <div>
         <h1>My Missions</h1>
         <ListGroup>
-          <ListGroup.Item>Cras justo odio</ListGroup.Item>
-          <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-          <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-          <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+          {
+            (missions.length > 0)
+              ? missions.map((mission) => (
+                <ListGroup.Item key={mission.mission_id}>{mission.mission_name}</ListGroup.Item>
+              )) : (
+                <Alert variant="danger">
+                  <Alert.Heading>Oh sorry! You&apos;ve not joined any missions!</Alert.Heading>
+                </Alert>
+              )
+          }
         </ListGroup>
       </div>
       <div>
         <h1>My Rockets</h1>
         <ListGroup>
           {
-            (rockets.length > 1)
+            (rockets.length > 0)
               ? rockets.map((rockets) => (
                 <ListGroup.Item key={rockets.id}>{rockets.name}</ListGroup.Item>
               )) : (
